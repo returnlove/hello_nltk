8c8a77241274d702d8095b9200832f444eac7199
returnlove <iamsainath2@gmail.com>
2017-01-08 23:29:24 +0530

    created word features

--- classification_example.py
+++ classification_example.py
@@ -19,10 +19,10 @@ for category in movie_reviews.categories():
 	for fileid in movie_reviews.fileids(category):
 		documents.append((list(movie_reviews.words(fileid)), category))
 
-# print(len(documents))
-print(documents[:1])
-# random.shuffle(documents)
-print(documents[:1])
+# # print(len(documents))
+# print(documents[:1])
+# # random.shuffle(documents)
+# print(documents[:1])
 
 
 all_words = []
@@ -30,4 +30,13 @@ all_words = []
 for w in movie_reviews.words():
 	all_words.append(w.lower())
 
-print(len(all_words))
+# print(len(all_words))
+
+# print(all_words[:2])
+# print(len(all_words))
+all_words = nltk.FreqDist(all_words)
+# print(len(all_words))
+
+print(all_words.most_common(10))
+
+print(all_words['stupid'])
